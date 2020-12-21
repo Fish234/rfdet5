6b5b1e17e61e0b15dc07a375c437931aa5b51e46
Natalia <hourglassbranch25@gmail.com>
2020-12-21 15:50:31 -0300

    scaffold Marker

---
+++ db/migrate/20201221184940_create_markers.rb
@@ -0,0 +1,11 @@
+class CreateMarkers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :markers do |t|
+      t.string :name
+      t.integer :type_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_184745) do
+ActiveRecord::Schema.define(version: 2020_12_21_184940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2020_12_21_184745) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "markers", force: :cascade do |t|
+    t.string "name"
+    t.integer "type_id"
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
